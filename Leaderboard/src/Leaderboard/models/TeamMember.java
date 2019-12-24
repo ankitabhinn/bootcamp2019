efeac5ed6b87254857eee5b2c7c5e3ac6c77c43b
SonaArun <sonavs2013@gmail.com>
2019-12-24 17:33:04 +0530

    day 2 files added

---
+++ Leaderboard/src/Leaderboard/models/TeamMember.java
@@ -0,0 +1,28 @@
+package Leaderboard.models;
+
+import java.util.List;
+
+public class TeamMember{
+	public int tid;
+	public String tname;
+	public List<MemberScore> members;
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+	public List<MemberScore> getMembers() {
+		return members;
+	}
+	public void setMembers(List<MemberScore> members) {
+		this.members = members;
+	}
+
+}
